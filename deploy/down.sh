96eba0b49a093acfeabe56f43b2a209b78798644
juhonamnam <jhnamnam@gmail.com>
2023-02-05 23:37:42 +0900

    modify build script

--- deploy/down.sh
+++ deploy/down.sh
@@ -4,7 +4,7 @@
 RUNNING_CONTAINERS=$(docker ps -q --filter ancestor=$APP_NAME:$APP_TAG)
 
 if [ ${#RUNNING_CONTAINERS} -ne 0 ]; then
-    echo "Stopping currently running containers..."
+    echo "exec: docker stop $RUNNING_CONTAINERS"
     docker stop $RUNNING_CONTAINERS
 fi
 
@@ -12,9 +12,10 @@ fi
 ALL_CONTAINERS=$(docker ps -a -q --filter ancestor=$APP_NAME:$APP_TAG)
 
 if [ ${#ALL_CONTAINERS} -ne 0 ]; then
-    echo "Deleting containers..."
+    echo "exec: docker rm $ALL_CONTAINERS"
     docker rm $ALL_CONTAINERS
 fi
 
 # Delete Image
+echo "exec: docker rmi -f $APP_NAME:$APP_TAG"
 docker rmi -f $APP_NAME:$APP_TAG
--- deploy/up.sh
+++ deploy/up.sh
@@ -2,6 +2,10 @@
 
 mkdir -p logs
 
+# Build Image
+echo "docker build --tag $APP_NAME:$APP_TAG ."
 docker build --tag $APP_NAME:$APP_TAG .
 
+# Run Container
+echo "exec: docker run --name $APP_NAME -v $(pwd)/logs:/trading-bot-project/logs -d $APP_NAME:$APP_TAG"
 docker run --name $APP_NAME -v $(pwd)/logs:/trading-bot-project/logs -d $APP_NAME:$APP_TAG
